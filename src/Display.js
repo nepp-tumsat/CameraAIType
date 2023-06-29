4a507504aaa1938cc103cd1e131692e51284afcc
RunaArima <irenelovelace000@gmail.com>
2023-06-30 00:54:24 +0900

    写真、文字の追加

--- src/Display.js
+++ src/Display.js
@@ -1,24 +1,29 @@
 import { Link } from "react-router-dom";
 import rogofin from './rogofin.png';
+import apple from './apple.jpg';
 import './Display.css';
 
 const Display = () => {
   return (
     <>
       <header>
-      <h1 class="headline">
-          <Link to="/">
-              <img src = {rogofin} width="192" alt="Camera AI Type"/>
-          </Link>
-      </h1>
-  </header>
-  <div class="btn">
-      <div className="batsu"><Link to="/" className="batsu1">×</Link></div>
-  </div>
-  <div class="nandb">
-        <a href="#" class="triangle-left"></a>
-        <a href="#" class="triangle-right"></a>
-  </div> 
+        <h1 class="headline">
+            <Link to="/">
+               <img src = {rogofin} width="192" alt="Camera AI Type"/>
+            </Link>
+        </h1>
+      </header>
+      <div class="btn">
+        <div className="hidari"><Link to="/" className="hidari1">←</Link></div>
+      </div>
+      <div class="tango">
+         <img src= {apple} width="500"/>
+        <h1>apple</h1>
+      </div>
+      {/* <div class="nandb">
+         <a href="#" class="triangle-left"></a>
+         <a href="#" class="triangle-right"></a>
+      </div>  */}
     </>
   );
 };
