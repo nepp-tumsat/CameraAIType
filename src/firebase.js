03ce0de4e05c5caf47375a5c07148e3ae56cd04e
hashimon000 <hashimon.hashimon@icloud.com>
2023-06-29 20:03:02 +0900

    firestoreの初期設定

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
 const firebaseConfig = {
   apiKey: process.env.apiKey,
   authDomain:  process.env.authDomain,
@@ -10,3 +11,5 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
